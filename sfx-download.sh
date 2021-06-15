81c1451c1e0007d45e3e6ea3c1b48514e4f6622d
Tomáš Cejpek <tomas.cejpek@email.cz>
2021-06-15 12:26:11 +0200

    sfx: add SVKUL

--- sfx-download.sh
+++ sfx-download.sh
@@ -7,7 +7,7 @@ else
   exit 1
 fi
 
-for id in {'VKOL','SVKPL','SVKHK','NKP','MZK','MSVK','MKP','KVKLI','KMHK','KKFB','JVKCB','KKKV','KKVysociny','UZEI'}; do
+for id in {'VKOL','SVKPL','SVKHK','NKP','MZK','MSVK','MKP','KVKLI','KMHK','KKFB','JVKCB','KKKV','KKVysociny','UZEI','SVKUL'}; do
   echo $id
   wget "https://sfx.knihovny.cz/sfxlcl3/cgi/public/get_file.cgi?file=institutional_holding-$id.xml"
 done
