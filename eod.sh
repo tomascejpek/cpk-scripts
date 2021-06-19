1b7edca0c5bcc41028ca3f16e89b3446aa3c19e0
Tomáš Cejpek <tomas.cejpek@email.cz>
2021-06-19 10:33:47 +0200

    eod download

---
+++ eod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/tomas/git/oai-harvester/
+
+bash harvester.sh --source=P1-UIBK --repeat=true &
+bash harvester.sh --source=P2-UT --repeat=true &
+bash harvester.sh --source=P3-NUK --repeat=true &
+bash harvester.sh --source=P5-EMAU --repeat=true &
+bash harvester.sh --source=P6-NLS --repeat=true &
+bash harvester.sh --source=P7-NCU --repeat=true &
+bash harvester.sh --source=P10-BNP --repeat=true &
+bash harvester.sh --source=P11-NLE --repeat=true &
+bash harvester.sh --source=P13-CVTISR --repeat=true &
+bash harvester.sh --source=P14-UREG --repeat=true &
+bash harvester.sh --source=P15-VU --repeat=true &
