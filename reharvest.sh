a081e72d9dfd0af4d7638694d82e2615d7a3f52c
Tomáš Cejpek <tomas.cejpek@email.cz>
2021-07-07 15:20:33 +0200

    fix reharvest

--- reharvest.sh
+++ reharvest.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export LC_ALL="cs_CZ.UTF-8"
+export TZ="Europe/Prague"
+
 rmPath=$(bash ini.sh getValue defaultRmPath)
 
 if [ -z $rmPath ]; then
@@ -10,5 +13,5 @@ cd $rmPath
 ids=(300)
 
 for id in "${ids[@]}"; do
-  nohup java -Dlogback.configurationFile=logback.xml -DCONFIG_DIR=config/ -jar target/cz.mzk.recordmanager.cmdline-1.0.0-SNAPSHOT.jar -job oaiHarvestJob -configurationId $id -reharvest true -repeat 1 &
+  nohup java -Dlogback.configurationFile=logback.xml -DCONFIG_DIR=config/ -jar target/cz.mzk.recordmanager.cmdline-1.0.0-SNAPSHOT.jar -job oaiHarvestJob -configurationId $id -reharvest true -repeat 1 > /logs/$id.log &
 done
