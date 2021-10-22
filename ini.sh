256cfbf5698bd678beb1fbda05658aed0c8a4014
Tomáš Cejpek <tomas.cejpek@email.cz>
2021-10-22 09:24:05 +0200

    update reharvest script

--- ini.sh
+++ ini.sh
@@ -5,9 +5,6 @@ iniFile="config.local.ini"
 getValue() {
   if grep "^$1=" $iniFile >/dev/null; then
     grep -oP "^$1=\K.*" $iniFile | xargs
-  else
-    printf "Unknown option\n"
-    exit 1
   fi
 }
 
--- reharvest.sh
+++ reharvest.sh
@@ -22,20 +22,30 @@ rmPath=$(bash ini.sh getValue defaultRmPath)
 logPath=$(bash ini.sh getValue defaultLogPath)
 threadCountMax=$(bash ini.sh getValue reharvestThreadCount)
 sources=$(bash ini.sh getValue reharvest$source)
+job=$(bash ini.sh getValue reharvestJob$source)
+async=$(bash ini.sh getValue reharvestAsync$source)
 
+if [ -z $job ]; then
+  job=$(bash ini.sh getValue reharvestJobDefault)
+fi
 if [ -z $rmPath ]; then
+  printf "Empty rm dir\n"
   exit 1
 fi
 cd $rmPath
 
 if [ -n "$sources" ]; then
-  echo $sources | grep "[0-9]*" -o | while read -r id ; do
+  echo $sources | grep "[0-9]*" -o | while read -r id; do
     threadCount=$(ps aux | grep "cz.mzk.recordmanager" | grep "java" | wc -l)
     while [ $threadCount -ge $threadCountMax ]; do
       sleep 30
       threadCount=$(ps aux | grep "cz.mzk.recordmanager" | grep "java" | wc -l)
     done
     echo "run $id"
-    nohup java -Dlogback.configurationFile=logback.xml -DCONFIG_DIR=config/ -jar target/cz.mzk.recordmanager.cmdline-1.0.0-SNAPSHOT.jar -job oaiHarvestJob -configurationId $id -reharvest true -repeat 1 > $logPath$id.log &
+    if [ "$async" == "no" ]; then
+      nohup java -Dlogback.configurationFile=logback.xml -DCONFIG_DIR=config/ -jar target/cz.mzk.recordmanager.cmdline-1.0.0-SNAPSHOT.jar -job $job -configurationId $id -reharvest true -repeat 1 >$logPath$id.log
+    else
+      nohup java -Dlogback.configurationFile=logback.xml -DCONFIG_DIR=config/ -jar target/cz.mzk.recordmanager.cmdline-1.0.0-SNAPSHOT.jar -job $job -configurationId $id -reharvest true -repeat 1 >$logPath$id.log &
+    fi
   done
 fi
