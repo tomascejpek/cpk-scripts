6a45a468314d05264873d655540bcc3288e8b11d
Tomáš Cejpek <tomas.cejpek@email.cz>
2022-03-31 09:44:21 +0200

    update sfx download

--- sfx-download.sh
+++ sfx-download.sh
@@ -7,10 +7,10 @@ else
   exit 1
 fi
 
-for id in {'VKOL','SVKPL','SVKHK','NKP','MZK','MSVK','MKP','KVKLI','KMHK','KKFB','JVKCB','KKKV','KKVysociny','UZEI','SVKUL','NLK'}; do
+for id in {'VKOL','SVKPL','SVKHK','NKP','MZK','MSVK','MKP','KVKLI','KMHK','KKFB','JVKCB','KKKV','KKVysociny','UZEI','SVKUL','NLK', 'SVKKL'}; do
   echo $id
-  wget "https://sfx.knihovny.cz/sfxlcl3/cgi/public/get_file.cgi?file=institutional_holding-$id.xml"
+  wget --no-check-certificate "https://sfx.knihovny.cz/sfxlcl3/cgi/public/get_file.cgi?file=institutional_holding-$id.xml"
 done
 
-wget "https://sfx.knihovny.cz/sfxlcl3/cgi/public/get_file.cgi?file=institutional_holding.xml"
-wget "https://sfx.techlib.cz/sfxlcl41/cgi/public/get_file.cgi?file=institutional_holding-NTK.xml"
+wget --no-check-certificate "https://sfx.knihovny.cz/sfxlcl3/cgi/public/get_file.cgi?file=institutional_holding.xml"
+wget --no-check-certificate "https://sfx.techlib.cz/sfxlcl41/cgi/public/get_file.cgi?file=institutional_holding-NTK.xml"
