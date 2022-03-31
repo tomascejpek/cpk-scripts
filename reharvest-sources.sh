5db4f96e0d121f462d63f69ee44c92473810f559
Tomáš Cejpek <tomas.cejpek@email.cz>
2022-03-31 09:46:16 +0200

    reharvest script

---
+++ reharvest-sources.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /logs/cpk-scripts/
+
+bash reharvest.sh --source=Arl
+bash reharvest.sh --source=Tritius
+bash reharvest.sh --source=Clavius
+bash reharvest.sh --source=Koha
+bash reharvest.sh --source=Kramerius
+bash reharvest.sh --source=Random
