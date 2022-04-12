e91a54e5a476d79ae599b679b4791487136f55cd
Tomáš Cejpek <tomas.cejpek@email.cz>
2022-04-12 13:47:30 +0200

    marcit

---
+++ marcit.sql
@@ -0,0 +1,18 @@
+with locs as
+         (select DISTINCT REPLACE(loc, ' ', '-') as loc
+          from harvested_record hr
+                       join loc l on hr.id=l.harvested_record_id
+              where import_conf_id=100001
+              UNION
+              select DISTINCT loc
+              from harvested_record hr
+                       join loc l on hr.id=l.harvested_record_id
+              where import_conf_id=100001
+         ), locs_others as (
+    select DISTINCT loc
+    from harvested_record hr
+             join loc l on hr.id=l.harvested_record_id
+    where import_conf_id!=100001
+)
+select DISTINCT loc from locs_others where loc not in (select loc from locs)
+
