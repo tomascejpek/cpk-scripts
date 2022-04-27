bf6e3d280fdb7b5e839ab8b38f8221210eedd0d7
Tomáš Cejpek <tomas.cejpek@email.cz>
2022-04-27 14:38:38 +0200

    fix typo

--- sfx-download.sh
+++ sfx-download.sh
@@ -7,7 +7,7 @@ else
   exit 1
 fi
 
-for id in {'VKOL','SVKPL','SVKHK','NKP','MZK','MSVK','MKP','KVKLI','KMHK','KKFB','JVKCB','KKKV','KKVysociny','UZEI','SVKUL','NLK', 'SVKKL'}; do
+for id in {'VKOL','SVKPL','SVKHK','NKP','MZK','MSVK','MKP','KVKLI','KMHK','KKFB','JVKCB','KKKV','KKVysociny','UZEI','SVKUL','NLK','SVKKL'}; do
   echo $id
   wget --no-check-certificate "https://sfx.knihovny.cz/sfxlcl3/cgi/public/get_file.cgi?file=institutional_holding-$id.xml"
 done
