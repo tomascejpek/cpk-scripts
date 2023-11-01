addfa36a63ff02367bfbc7fe1ee683efbdc98266
Tomáš Cejpek <tomas.cejpek@email.cz>
2023-11-01 13:01:13 +0100

    update sql

--- deleteDedupKeys.sql
+++ deleteDedupKeys.sql
@@ -19,6 +19,6 @@ DELETE FROM bl_language where harvested_record_id in (select id from harvested_r
 DELETE FROM anp_title where harvested_record_id in (select id from harvested_record where deleted is not null and next_dedup_flag is true and import_conf_id not in (316,400,344) and palmknihy_id is null);
 DELETE FROM uuid where harvested_record_id in (select id from harvested_record where deleted is not null and next_dedup_flag is true and import_conf_id not in (316,400,344) and palmknihy_id is null);
 
-select import_conf_id,count(*) from harvested_record where deleted is not null and dedup_keys_hash is not null and dedup_keys_hash!='' and import_conf_id not in (316,400,344) group by import_conf_id;
+select import_conf_id,count(*) from harvested_record where deleted is not null and dedup_keys_hash is not null and dedup_keys_hash!='' and import_conf_id not in (316,400,344) and palmknihy_id is null group by import_conf_id;
 
 update harvested_record set next_dedup_flag=true where deleted is not null and dedup_keys_hash is not null and dedup_keys_hash!='' and import_conf_id not in (316,400,344,333) and palmknihy_id is null;
