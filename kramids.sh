16364f4470f43c313ea0967cdfafcd7e8db70ab0
Tomáš Cejpek <tomas.cejpek@email.cz>
2025-08-15 09:38:18 +0200

    update kramerius ids harvester

--- kramids.sh
+++ kramids.sh
@@ -25,24 +25,26 @@ else
   mkdir $dataPath
 fi
 
+cursorMark='*'
 if [ "$source" == "kram-nkp" ]; then
-  url="$sourceUrl/search/api/v5.0/search?q=*:*&fq=fedora.model:monograph+OR+fedora.model:periodical+OR+fedora.model:map+OR+fedora.model:archive+OR+fedora.model:manuscript+OR+fedora.model:sheetmusic+OR+fedora.model:soundrecording+OR+fedora.model:graphic+OR+fedora.model:convolute&fl=PID&rows=$count&start="
+  url="$sourceUrl/search/api/v5.0/search?q=*:*&fq=fedora.model:monograph+OR+fedora.model:periodical+OR+fedora.model:map+OR+fedora.model:archive+OR+fedora.model:manuscript+OR+fedora.model:sheetmusic+OR+fedora.model:soundrecording+OR+fedora.model:graphic+OR+fedora.model:convolute&fl=PID&rows=$count&sort=PID+asc"
 elif [ "$source" == "kram-mzk" ]; then
-  url="$sourceUrl/search/api/client/v7.0/search?q=*:*&fq=model:monograph+OR+model:periodical+OR+model:map+OR+model:archive+OR+model:manuscript+OR+model:sheetmusic+OR+model:soundrecording+OR+model:graphic+OR+model:convolute&fl=pid&wt=xml&rows=$count&start="
+  url="$sourceUrl/search/api/client/v7.0/search?q=*:*&fq=model:monograph+OR+model:periodical+OR+model:map+OR+model:archive+OR+model:manuscript+OR+model:sheetmusic+OR+model:soundrecording+OR+model:graphic+OR+model:convolute&fl=pid&wt=xml&rows=$count&sort=pid+asc"
 fi
 
 now=$(date)
 error=0
-echo "$now: Downloading URL: $url$i" >$logPath'kram.log'
-curl -s -k "$url$i" >$dataPath/temp
+echo "$now: Downloading URL: $url&cursorMark=$cursorMark" >$logPath'kram.log'
+curl -s -k "$url&cursorMark=$cursorMark" >$dataPath/temp
 
 while true; do
   now=$(date)
-  echo "$now: Downloading URL: $url$i" >>$logPath'kram.log'
-  curl -s -k "$url$i" >$dataPath/temp
+  echo "$now: Downloading URL: $url&cursorMark=$cursorMark" >>$logPath'kram.log'
+  curl -s -k "$url&cursorMark=$cursorMark" >$dataPath/temp
   if grep "str name=\"PID\"" -i $dataPath/temp >/dev/null; then
     ((i += $count))
     cat $dataPath/temp >$dataPath/$i.xml
+    cursorMark=$(grep -oP "nextCursorMark\">\K[^<]*" $dataPath/temp)
   else
     echo "$now: DONE" >>$logPath'kram.log'
     break
